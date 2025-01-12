50ef2ed2521c29d4cb4cb11cae5160fd99d2f0fc
nurhadfina <nrhadfina1510@gmail.com>
2025-01-12 18:23:33 +0800

    commit for visit penang

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import "./Navbar.css";
 
 const Navbar = () => (
   <nav className="navbar">
-    <input class="search-bar" type="text" placeholder="Search"></input>
+    <input className="search-bar" type="text" placeholder="Search" />
     <ul>
-      <li><a href="/hero">Home</a></li>
-      <li><a href="/tourist-spots">Tourist Spots</a></li>
-      <li><a href="/food">Food & Beverages</a></li>
-      <li><a href="/hotels">Hotels</a></li>
+      <li><a href="#hero">Home</a></li>
+      <li><a href="#tourist-spots">Tourist Spots</a></li>
+      <li><a href="#food">Food & Beverages</a></li>
+      <li><a href="#hotels">Hotels</a></li>
     </ul>
   </nav>
 );
 
-export default Navbar;
+export default Navbar; 
